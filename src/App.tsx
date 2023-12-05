a6339bf91bf3f498389b663a2f363a8255550a95
vitaly.krasnov <Vitaly.Krasnov@x5.ru>
2023-12-06 02:15:05 +0300

    правка шаблона

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,17 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <div className="App-container">
           <h3>Хочу достичь следующих целей:</h3>
-          <ul>
+          <ul className="App-ul">
             <li>погрузиться в разработку фронтенда на React</li>
           </ul>
 
           <h3>Владею следующими технологиями:</h3>
-          <ul>
+          <ul className="App-ul"></div>>
             <li>backend на java и немного css/html/javascript</li>
           </ul>
 
           <h3>О себе:</h3>
-          <ul>
+          <ul className="App-ul">>
             <li>довольно давно на java backend, на проекте возникла задача по переносу клиентской части приложения
                на html/javascript и стало интересно как устроен современный фронтенд</li>
           </ul>
