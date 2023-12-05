fd5b84c4b3b260275f83db3612a4bbd634137da1
vitaly.krasnov <Vitaly.Krasnov@x5.ru>
2023-12-06 01:43:04 +0300

    добавлена информация о цели обучения

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,21 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Текст писать тут
+          <h3>Хочу достичь следующих целей:</h3>
+          <ul>
+            <li>погрузиться в разработку фронтенда на React</li>
+          </ul>
+
+          <h3>Владею следующими технологиями:</h3>
+          <ul>
+            <li>backend на java и немного css/html/javascript</li>
+          </ul>
+
+          <h3>О себе:</h3>
+          <ul>
+            <li>довольно давно на java backend, на проекте возникла задача по переносу клиентской части приложения
+               на html/javascript и стало интересно как устроен современный фронтенд</li>
+          </ul>
         </p>
       </header>
     </div>
